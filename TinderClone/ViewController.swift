7140e4473efbf8034e21f187d4aa14bce76f8c7a
Felipe Cisotto de Camargo <felipe.cisotto.camargo@everis.com>
2020-04-04 13:02:07 -0300

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Felipe Cisotto on 04/04/20.
+//  Copyright © 2020 Felipe Cisotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
